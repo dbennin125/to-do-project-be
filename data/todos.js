379d056fa9bfe2258d3daaf94cfa34fd88425431
Daniel Bennington <dbennington125@gmail.com>
2020-05-12 13:55:53 -0700

    data almost ready to drops tables

---
+++ data/todos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    item: 'walk dog',
+    importance: 10,
+  },
+  {
+    item: 'shopping',
+    importance: 4,
+  },
+  {
+    item: 'get booze',
+    importance: 6,
+  }
+];
